7eef6746884b0d04ba749026aed7204b341f0f51
BhuvanNagesh <109874160+BhuvanNagesh@users.noreply.github.com>
2026-04-14 03:37:37 +0530

    Delete module11-arya directory

--- module11-arya/src/main/java/com/wealthwise/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.wealthwise.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.wealthwise.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
